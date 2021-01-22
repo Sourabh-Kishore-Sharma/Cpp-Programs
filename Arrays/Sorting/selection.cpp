4f7500c6bcbdb0ec6647c89f5abe0d99db26e89b
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2021-01-22 16:22:03 +0530

    Added sorting logic within main.

---
+++ Arrays/Sorting/selection.cpp
@@ -0,0 +1,32 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+  int n;
+
+  cout<<"Enter array size: ";
+  cin>>n;
+
+  int arr[n];
+  cout<<"Enter elements: ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int temp;
+  for(int i=0;i<n-1;i++){
+    for(int j=i+1; j<n;j++){
+      if(arr[j] < arr[i]){
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  cout<<"Sorted array is ";
+
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+}
