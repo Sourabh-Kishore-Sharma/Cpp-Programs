bbdc6de56bfcc38610f8f64c509b5193715afd0c
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2021-02-20 21:49:58 +0530

    Program to check entered word is palindrome or not.

---
+++ Arrays/Character/palindrome.cpp
@@ -0,0 +1,29 @@
+/*
+Program to check whether entered string
+is palindrome or not.
+*/
+#include "iostream"
+#include "cstring"
+using namespace std;
+
+int main(){
+
+  char arr[50];
+  cin>>arr;
+
+  int len = strlen(arr);
+
+  bool check = true;
+
+  for(int i=0; i<len; i++){
+    if(arr[i] != arr[len-1-i]){
+      cout<<arr[i]<<" "<<arr[len-1-i];
+      check=false;
+      break;
+    }
+  }
+  if(check == true)
+    cout<<"It is a palindrome."<<endl;
+  else
+    cout<<"Not a palindrome."<<endl;
+}
