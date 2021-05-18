62408f524043d7d154b377bbe9bae5f5c7e56312
Sourabh <sourabhsharma1999s@gmail.com>
2021-05-18 21:49:56 +0530

    Example of call-by-reference.

---
+++ Pointers/ptr_to_fnc.cpp
@@ -0,0 +1,21 @@
+/*
+Example of call-by-reference
+*/
+
+#include "iostream"
+using namespace std;
+
+void swap(int* a, int* b){
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int main(){
+  int a,b;
+
+  cin>>a>>b;
+  cout<<"a="<<a<<" "<<"b="<<b<<endl;
+  swap(&a,&b);
+  cout<<"a="<<a<<" "<<"b="<<b<<endl;
+}
