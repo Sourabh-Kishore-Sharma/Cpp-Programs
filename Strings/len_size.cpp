ce4997a0682e07e9b002ca8ad48f8e4de01aa97a
Sourabh <sourabhsharma1999s@gmail.com>
2021-05-24 09:09:46 +0530

    Exmaple of length() and size().

---
+++ Strings/len_size.cpp
@@ -0,0 +1,17 @@
+/*
+length() $ size() function returns you the length of the string.
+*/
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main(){
+  string str;
+
+  str = "Sourabh Sharma";
+
+  cout<<str.length()<<endl;
+  cout<<str.size()<<endl;
+
+}
