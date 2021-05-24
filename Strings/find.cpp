8e4fdb29b4ab44b843b7bf071f62b327808c02e6
Sourabh <sourabhsharma1999s@gmail.com>
2021-05-24 08:38:57 +0530

    Example of find function in strings.

---
+++ Strings/find.cpp
@@ -0,0 +1,14 @@
+/*
+The find function returns the first index of the substring if found
+*/
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main(){
+  string str = "An example of find function.";
+
+  cout<<str.find("find")<<endl;
+
+}
