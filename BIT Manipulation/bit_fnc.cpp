c977d406b01b076ff353fb555a4ad5fdff0455a9
Sourabh <sourabhsharma1999s@gmail.com>
2021-05-29 07:46:34 +0530

    Added menu to choose functions from.

--- BIT Manipulation/bit_fnc.cpp
+++ BIT Manipulation/bit_fnc.cpp
@@ -24,11 +24,27 @@ int setBit(int n, int pos){
 
 int main(){
   int n, pos;
-  cin>>n>>pos;
 
-  cout<<getBit(n, pos)<<endl;
 
-  cout<<setBit(n, pos)<<endl;
+  cout<<"1. Get Bit\n"
+        "2. Set Bit to 1\n"
+        "3. Clear Bit (Set Bit to 0)\n";
+
+  int c;
+  cout<<"Enter Option : ";
+  cin>>c;
+
+  switch(c){
+    case 1:
+      cin>>n>>pos;
+      cout<<getBit(n, pos)<<endl;
+      break;
+
+    case 2:
+      cin>>n>>pos;
+      cout<<setBit(n, pos)<<endl;
+      break;
+  }
 
   return 0;
 }
