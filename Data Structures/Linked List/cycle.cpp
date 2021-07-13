b3a29e0b30f887a31d7b89700ddfb160756d90f9
Sourabh <sourabhsharma1999s@gmail.com>
2021-07-13 08:07:11 +0530

    Added program to detect any presence of a cycle in a linked list.

---
+++ Data Structures/Linked List/cycle.cpp
@@ -0,0 +1,85 @@
+#include "iostream"
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+
+        node(int val){
+            data = val;
+            next = NULL;
+        }
+};
+
+void insert(node* &head, int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+    return;
+}
+
+void makeCycle(node* head, int pos){
+    node* temp = head;
+    node* start_node;
+
+    int count = 1;
+
+    while(temp->next != NULL){
+        if(count==pos)
+            start_node = temp;
+
+        temp = temp->next;
+        count++;
+    }
+
+    temp->next = start_node;
+    return;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+bool detectCycle(node* head){
+    node* slow = head;
+    node* fast = head;
+
+    while(fast!=NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow==fast)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    node* head = NULL;
+
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    insert(head, 4);
+    insert(head, 5);
+
+    makeCycle(head, 3);
+
+    //display(head);
+
+    cout<<detectCycle(head)<<endl;
+}
