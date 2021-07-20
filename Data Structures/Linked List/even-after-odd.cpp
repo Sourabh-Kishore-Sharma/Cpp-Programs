a7627c3a99f49cb8ed5e4a2b18c31b55530d77af
Sourabh <sourabhsharma1999s@gmail.com>
2021-07-20 08:51:47 +0530

    Put odd position nodes after even position nodes.

---
+++ Data Structures/Linked List/even-after-odd.cpp
@@ -0,0 +1,83 @@
+/*
+Program to put even position nodes after odd position nodes.
+*/
+#include "iostream"
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+
+        node(int val){
+            data = val;
+            next = NULL;
+        }
+};
+
+void insert(node* &head, int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node* head){
+    if(head==NULL)
+        return;
+
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void evenAfterOdd(node* &head){
+    if(head->next==NULL)
+        return;
+    
+    node* odd = head;
+    node* even = head->next;
+    node* evenStart = even;
+
+    while(odd->next!=NULL && even->next!=NULL){
+
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+
+    }
+    odd->next = evenStart;
+
+    //If there are odd number of nodes
+    if(odd->next==NULL){
+        even->next = NULL;
+    }
+}
+
+int main(){
+    node* head = NULL;
+    
+    int arr[] = {1,2,3,4,5};
+
+    for(int i=0; i<6; i++){
+        insert(head, arr[i]);
+    }
+
+    display(head);
+
+    evenAfterOdd(head);
+
+    display(head);
+}
